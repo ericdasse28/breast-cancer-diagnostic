1b6cceeb616b1e237251d62f3e13a9132353ad28
Eric Dasse <christ.gououi@gmail.com>
2026-05-30 00:42:24 +0200

    refactor: change type hint

--- breast_cancer_diagnostic/evaluate.py
+++ breast_cancer_diagnostic/evaluate.py
@@ -4,7 +4,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score
 from breast_cancer_diagnostic.models import Model
 
 
-def evaluate(model: Model, X_test: pd.DataFrame, y_test: pd.DataFrame) -> dict:
+def evaluate(model: Model, X_test: pd.DataFrame, y_test: pd.Series) -> dict:
     y_pred = model.predict(X_test)
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred, pos_label="M")
--- breast_cancer_diagnostic/train.py
+++ breast_cancer_diagnostic/train.py
@@ -3,6 +3,6 @@ import pandas as pd
 from breast_cancer_diagnostic.models import Model
 
 
-def train(model: Model, X_train: pd.DataFrame, y_train: pd.DataFrame) -> Model:
+def train(model: Model, X_train: pd.DataFrame, y_train: pd.Series) -> Model:
     model.fit(X_train, y_train)
     return model
