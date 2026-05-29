280cfbe50011da70dc47ad45151ee606a6633fcc
Eric Dasse <christ.gououi@gmail.com>
2026-05-29 22:30:53 +0200

    feat: train takes model as an argument

--- breast_cancer_diagnostic/train.py
+++ breast_cancer_diagnostic/train.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from typing import Protocol
 
 import numpy as np
-from sklearn.linear_model import LogisticRegression
 
 
 class Model(Protocol):
@@ -12,7 +11,6 @@ class Model(Protocol):
     def predict_proba(self, X: np.ndarray) -> np.ndarray: ...
 
 
-def train(X: np.ndarray, y: np.ndarray) -> Model:
-    model = LogisticRegression()
+def train(model: Model, X: np.ndarray, y: np.ndarray) -> Model:
     model.fit(X, y)
     return model
