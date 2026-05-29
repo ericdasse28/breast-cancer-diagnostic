d04f74977baa1ac1c800a2464c30e19ad94383c1
Eric Dasse <christ.gououi@gmail.com>
2026-05-29 22:46:51 +0200

    refactor(train): rename arguments

--- breast_cancer_diagnostic/train.py
+++ breast_cancer_diagnostic/train.py
@@ -1,8 +1,8 @@
-import numpy as np
+import pandas as pd
 
 from breast_cancer_diagnostic.models import Model
 
 
-def train(model: Model, X: np.ndarray, y: np.ndarray) -> Model:
-    model.fit(X, y)
+def train(model: Model, X_train: pd.DataFrame, y_train: pd.DataFrame) -> Model:
+    model.fit(X_train, y_train)
     return model
